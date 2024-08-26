b977d5e87aa4ec5d9819c7d8b116a7238fe0a171
614project <71429443+614project@users.noreply.github.com>
2024-08-27 03:19:59 +0900

    초기 커밋.

---
+++ Exception.cs
@@ -0,0 +1,17 @@
+﻿namespace Algorizem;
+
+/// <summary>
+/// Algorizem에서 발생한 예외입니다.
+/// </summary>
+public class AlgorizemException : Exception
+{
+    /// <summary>
+    /// Algorizem 예외입니다.
+    /// </summary>
+    public AlgorizemException() : base() { }
+    /// <summary>
+    /// Algorizem 예외입니다.
+    /// </summary>
+    /// <param name="message">발생 원인</param>
+    public AlgorizemException(string message) : base(message) { }
+}
